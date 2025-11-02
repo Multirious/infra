da89936b03ca392fc2f4d13dd39afb3fcff022ab
Multirious <multirious@outlook.com>
2025-11-02 17:25:55 +0700

    Make wofi single file

--- modules/wofi.nix
+++ modules/wofi.nix
@@ -13,7 +13,81 @@ top: {
       home.packages = [
         pkgs.wofi
       ];
-      home.file.".config/wofi/config".source = ./wofi/config;
-      home.file.".config/wofi/style.css".source = ./wofi/style.css;
+
+      home.file.".config/wofi/config".text = ''
+        show=drun
+        allow_images=true
+        allow_markup=true
+        hide_scroll=true
+        matching=fuzzy
+        insensitive=true
+        prompt=search
+
+        no_actions=true
+        image_size=25
+        single_click=true
+        filter_rate=50
+      '';
+
+      home.file.".config/wofi/style.css".text =
+        # css
+        ''
+          * {
+            font-family: "RobotoMono Nerd Font";
+            /* font-weight: regular; */
+            font-size: 15px;
+          }
+
+          /* The name of the window itself */
+          #window {
+            background-color: rgba(24, 24, 24, 0.80);
+            box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1);
+            border-radius: 1rem;
+            font-size: 1.2rem;
+            /* The name of the box that contains everything */
+          }
+          #window #outer-box {
+            /* The name of the search bar */
+            /* The name of the scrolled window containing all of the entries */
+          }
+          #window #outer-box #input {
+            background-color: rgba(24, 24, 24, 0.6);
+            color: #f2f2f2;
+            border: none;
+            border-bottom: 1px solid rgba(24, 24, 24, 0.2);
+            padding: 0.8rem 1rem;
+            font-size: 1.5rem;
+            border-radius: 1rem 1rem 0 0;
+          }
+          #window #outer-box #input:focus, #window #outer-box #input:focus-visible, #window #outer-box #input:active {
+            border: none;
+            outline: 2px solid transparent;
+            outline-offset: 2px;
+          }
+          #window #outer-box #scroll {
+            /* The name of the box containing all of the entries */
+          }
+          #window #outer-box #scroll #inner-box {
+            /* The name of all entries */
+            /* The name of all boxes shown when expanding  */
+            /* entries with multiple actions */
+          }
+          #window #outer-box #scroll #inner-box #entry {
+            color: #fff;
+            background-color: rgba(24, 24, 24, 0.1);
+            padding: 0.6rem 1rem;
+            /* The name of all images in entries displayed in image mode */
+            /* The name of all the text in entries */
+          }
+          #window #outer-box #scroll #inner-box #entry #img {
+            width: 1rem;
+            margin-right: 0.5rem;
+          }
+          #window #outer-box #scroll #inner-box #entry:selected {
+            color: #fff;
+            background-color: rgba(255, 255, 255, 0.1);
+            outline: none;
+          }
+        '';
     };
 }
