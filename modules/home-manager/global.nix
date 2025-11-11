cbe1eb3f04c59202721f4ea16c4aaf0598f7d324
Multirious <multirious@outlook.com>
2025-11-12 00:28:14 +0700

    Disable genericLinux by default

--- modules/home-manager/global.nix
+++ modules/home-manager/global.nix
@@ -3,7 +3,6 @@ top: {
     { pkgs, ... }:
     {
       home.packages = [ pkgs.home-manager ];
-      targets.genericLinux.enable = true;
       nixpkgs.config.allowUnfree = true;
       home.stateVersion = "24.05";
       news.display = "silent";
