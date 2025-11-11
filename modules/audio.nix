c26e6dbffa3791bcb45eb17d7896b2e4ce9a12fb
Multirious <multirious@outlook.com>
2025-11-12 02:48:05 +0700

    Make module declaration in audio.nix consistent

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,22 +1,24 @@
 top: {
   configurations.nixos.peach-asus.use = m: [ m.audio ];
 
-  flake.modules.nixos.audio = {
-    services.pipewire.enable = true;
-    services.pipewire.pulse.enable = true;
-    security.pam.loginLimits = [
-      {
-        domain = "@audio";
-        item = "rtprio";
-        type = "-";
-        value = "95";
-      }
-      {
-        domain = "@audio";
-        item = "memlock";
-        type = "-";
-        value = "unlimited";
-      }
-    ];
-  };
+  flake.modules.nixos.audio =
+    { ... }:
+    {
+      services.pipewire.enable = true;
+      services.pipewire.pulse.enable = true;
+      security.pam.loginLimits = [
+        {
+          domain = "@audio";
+          item = "rtprio";
+          type = "-";
+          value = "95";
+        }
+        {
+          domain = "@audio";
+          item = "memlock";
+          type = "-";
+          value = "unlimited";
+        }
+      ];
+    };
 }
