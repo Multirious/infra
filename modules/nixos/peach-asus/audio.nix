254fb9823221b090b341713deb63152382086c9d
Multirious <multirious@outlook.com>
2025-11-12 19:44:48 +0700

    Fix typo

--- modules/nixos/peach-asus/audio.nix
+++ modules/nixos/peach-asus/audio.nix
@@ -1,5 +1,5 @@
 top: {
-  configuration.nixos.peach-asus.module =
+  configurations.nixos.peach-asus.module =
     { ... }:
     {
       environment.etc."wireplumber/wireplumber.conf.d".source = ./wireplumber.conf.d;
