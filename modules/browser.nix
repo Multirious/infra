9953a230196787a96468609895de461473aa0af1
Multirious <multirious@outlook.com>
2025-11-13 23:36:36 +0700

    Format browser module

--- modules/browser.nix
+++ modules/browser.nix
@@ -4,37 +4,39 @@ top: {
   flake.modules.homeManager.browser =
     { config, pkgs, ... }:
     {
-      home.packages = 
+      home.packages =
         let
-          librewolf = pkgs.librewolf.overrideAttrs (a:
-            {
-              buildCommand = a.buildCommand + ''
-                wrapProgram "$executablePath" \
-                  --set 'HOME' '${config.home.homeDirectory}/.local/share/librewolf'
-              '';
-            }
-          );
-          tor-browser = (import (builtins.fetchGit {
-            name = "nixpkgs-with-tor-browser-v14.0.9";
-            url = "https://github.com/NixOS/nixpkgs/";
-            ref = "refs/heads/nixpkgs-unstable";
-            rev = "7d7ba194bf834a5194dadfa8f9debcfabaa718bb";
-          }) {
-            inherit (pkgs.stdenv.hostPlatform) system;
-          }).tor-browser;
+          librewolf = pkgs.librewolf.overrideAttrs (a: {
+            buildCommand = a.buildCommand + ''
+              wrapProgram "$executablePath" \
+                --set 'HOME' '${config.home.homeDirectory}/.local/share/librewolf'
+            '';
+          });
+          tor-browser =
+            (import
+              (builtins.fetchGit {
+                name = "nixpkgs-with-tor-browser-v14.0.9";
+                url = "https://github.com/NixOS/nixpkgs/";
+                ref = "refs/heads/nixpkgs-unstable";
+                rev = "7d7ba194bf834a5194dadfa8f9debcfabaa718bb";
+              })
+              {
+                inherit (pkgs.stdenv.hostPlatform) system;
+              }
+            ).tor-browser;
         in
         [
           tor-browser
-          librewolf 
+          librewolf
           pkgs.google-chrome
         ];
       xdg.mimeApps = {
         defaultApplications = {
-          "application/pdf"          = [ "librewolf.desktop" ];
-          "text/html"                = [ "librewolf.desktop" ];
-          "x-scheme-handler/http"    = [ "librewolf.desktop" ];
-          "x-scheme-handler/https"   = [ "librewolf.desktop" ];
-          "x-scheme-handler/about"   = [ "librewolf.desktop" ];
+          "application/pdf" = [ "librewolf.desktop" ];
+          "text/html" = [ "librewolf.desktop" ];
+          "x-scheme-handler/http" = [ "librewolf.desktop" ];
+          "x-scheme-handler/https" = [ "librewolf.desktop" ];
+          "x-scheme-handler/about" = [ "librewolf.desktop" ];
           "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
         };
       };
