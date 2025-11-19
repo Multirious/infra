b12f5c1378c0ebf1a7420131484b819a92f8888b
Multirious <multirious@outlook.com>
2025-11-19 16:35:30 +0700

    Add GE-Proton

--- modules/games.nix
+++ modules/games.nix
@@ -4,15 +4,16 @@ top: {
   flake.modules.homeManager.games =
     { config, pkgs, ... }:
     {
-      home.packages =
-        let
-          steam = pkgs.steam.override {
-            extraEnv.HOME = "${config.home.homeDirectory}/.local/share/steam";
-          };
-        in
-        [
-          steam
-          # pkgs.prismlauncher
-        ];
+      home.packages = [
+        (pkgs.steam.override {
+          extraEnv.HOME = "${config.xdg.dataHome}/steam";
+        })
+        pkgs.protontricks
+        # pkgs.prismlauncher
+      ];
+      xdg.dataFile."Steam/compatibilitytools.d/GE-Proton10-25".source = builtins.fetchTarball {
+        url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-25/GE-Proton10-25.tar.gz";
+        sha256 = "0jb3j9ida2962nmpihpjbp5da70q20hls1r0sn17pdii0ghjiaa4";
+      };
     };
 }
