54fdf556256c63dddb642d06e911ac45403d20f8
Multirious <multirious@outlook.com>
2025-11-19 16:37:19 +0700

    Make gnome online accounts user mode

--- modules/online-accounts.nix
+++ modules/online-accounts.nix
@@ -1,11 +1,22 @@
 top: {
   configurations.nixos.peach-asus.use = m: [ m.online-accounts ];
+  configurations.homeManager.peach.use = m: [ m.online-accounts ];
 
   flake.modules.nixos.online-accounts =
+    { ... }:
+    {
+      services.gnome.gnome-keyring.enable = true;
+    };
+
+  flake.modules.homeManager.online-accounts =
     { pkgs, ... }:
     {
-      environment.systemPackages = [ pkgs.gnome-online-accounts-gtk ];
-      services.gnome.gnome-online-accounts.enable = true;
-      services.gnome.gnome-keyring.enable = true;
+      home.packages = [
+        pkgs.gnome-online-accounts-gtk
+        pkgs.gnome-online-accounts
+      ];
+      dbus.packages = [
+        pkgs.gnome-online-accounts
+      ];
     };
 }
