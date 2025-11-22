a71980c1cb71a9e2c45fd90fc085c1df7541993d
Multirious <multirious@outlook.com>
2025-11-22 08:48:43 +0700

    Move zsh script for interactive mode to interative module
    Fix wrong attribute location
    
    Remove unused inherit

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -2,7 +2,7 @@ top: {
   flake.modules.homeManager.shell =
     { config, ... }:
     let
-      inherit (config.xdg) configHome stateHome cacheHome;
+      inherit (config.xdg) configHome;
     in
     {
       home.file.".zshenv".text =
@@ -30,50 +30,6 @@ top: {
           # standardize shell configurations
 
           . ${configHome}/shell/zsh/login
-        '';
-
-      xdg.configFile."shell/zsh/completion.zsh".text =
-        # zsh
-        ''
-          autoload -U compinit
-          compinit -d "${stateHome}/zsh/compdump"
-
-          _comp_options+=(globdots) # With hidden files
-
-          zstyle ':completion:*' completer _extensions _complete _approximate
-          zstyle ':completion:*' use-cache on
-          zstyle ':completion:*' cache-path "${cacheHome}/zsh/zcompcache"
-          zstyle ':completion:*' menu select
-        '';
-
-      xdg.configFile."shell/zsh/plugins.zsh".text =
-        # zsh
-        ''
-          source <(fzf --zsh)
-
-          source ${top.inputs.zsh-helix-mode}/zsh-helix-mode.plugin.zsh
-          bindkey -M hxins "jk" zhm_normal
-
-          source ${top.inputs.zsh-autosuggestions}/zsh-autosuggestions.plugin.zsh
-          ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(
-            zhm_history_prev
-            zhm_history_next
-            zhm_prompt_accept
-            zhm_accept
-            zhm_accept_or_insert_newline
-          )
-          ZSH_AUTOSUGGEST_ACCEPT_WIDGETS+=(
-            zhm_move_right
-            zhm_clear_selection_move_right
-          )
-          ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(
-            zhm_move_next_word_start
-            zhm_move_next_word_end
-          )
-
-          source ${top.inputs.zsh-syntax-highlighting}/zsh-syntax-highlighting.zsh
-
-          zhm-add-update-region-highlight-hook
         '';
     };
 }
