51450562ae6f7414fff019f448f101c798ecca90
Multirious <multirious@outlook.com>
2025-11-24 13:46:15 +0700

    Refactor

--- modules/audio-plugins.nix
+++ modules/audio-plugins.nix
@@ -10,6 +10,7 @@ top: {
     }:
     let
       inherit (pkgs.stdenv.hostPlatform) system;
+      myPackages = top.config.flake.packages."${system}";
     in
     {
       options =
@@ -45,11 +46,7 @@ top: {
 
         me.audio.vstFile."Vital.so".source = "${pkgs.vital}/lib/vst/Vital.so";
         me.audio.vst3File."Vital.vst3".source = "${pkgs.vital}/lib/vst3/Vital.vst3";
-        me.audio.vstFile."libsitala.so".source =
-          let
-            sitala = top.config.flake.packages."${system}".sitala;
-          in
-          "${sitala}/lib/vst/libsitala.so";
+        me.audio.vstFile."libsitala.so".source = "${myPackages.sitala}/lib/vst/libsitala.so";
 
         xdg.dataFile =
           (lib.mapAttrs' (path: value: {
