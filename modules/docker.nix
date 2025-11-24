cc255748cc563d8f7c854055dfe2ea2cab3a52f4
Multirious <multirious@outlook.com>
2025-11-24 09:17:49 +0700

    Add docker

---
+++ modules/docker.nix
@@ -0,0 +1,14 @@
+top: {
+  configurations.nixos.peach-asus.use = m: [ m.docker ];
+  configurations.nixos.peach-asus.module =
+    { ... }:
+    {
+      users.users.peach.extraGroups = [ "docker" ];
+    };
+
+  flake.modules.nixos.docker =
+    { ... }:
+    {
+      virtualisation.docker.enable = true;
+    };
+}
