7f3b5e71a1f418e07eb07b04a69666a6b1a4aca2
Multirious <multirious@outlook.com>
2025-11-25 18:15:41 +0700

    Add swaylock

---
+++ modules/desktop/swaylock.nix
@@ -0,0 +1,62 @@
+top: {
+  flake.modules.homeManager.hyprland.imports = [
+    top.config.flake.modules.homeManager.swaylock
+  ];
+  flake.modules.homeManager.sway.imports = [
+    top.config.flake.modules.homeManager.swaylock
+  ];
+
+  flake.modules.homeManager.swaylock =
+    { ... }:
+    {
+      xdg.configFile."swaylock/config".text = ''
+        fade-in=1
+        grace=0
+        indicator-idle-visible
+        clock
+        image=/home/peach/.local/share/backgrounds/desert_lake.jpg
+        font=RobotoMono Nerd Font
+        font-size=30
+        show-keyboard-layout
+        separator-color=ffffff00
+        key-hl-color=ffa500
+        bs-hl-color=ff644f
+        caps-lock-key-hl-color=ffa500
+        caps-lock-bs-hl-color=ff644f
+        indicator-caps-lock
+        indicator-radius=100
+        indicator-thickness=5
+        indicator-x-position=1750
+        indicator-y-position=860
+        inside-color=ffffff77
+        inside-clear-color=ffffff77
+        inside-ver-color=ffffff77
+        inside-wrong-color=ffffff77
+        inside-caps-lock-color=d1feb877
+        ring-color=eeeeee
+        ring-clear-color=eeeeee
+        ring-ver-color=4fa7ff
+        ring-wrong-color=ff644f
+        ring-caps-lock-color=eeeeee
+        line-color=ffffff00
+        line-clear-color=ffffff00
+        line-ver-color=ffffff00
+        line-wrong-color=ffffff00
+        line-caps-lock-color=ffffff00
+        text-color=444444
+        text-clear-color=444444
+        text-ver-color=444444
+        text-wrong-color=444444
+        text-caps-lock-color=444444
+        text-clear=
+        text-ver=
+        text-wrong=
+        text-caps-lock=󰘲
+        layout-border-color=ffffff00
+        layout-bg-color=ffffff00
+        layout-text-color=444444bb
+        effect-blur=20x7
+        effect-pixelate=20
+      '';
+    };
+}
