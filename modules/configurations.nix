77bc74170781202610bc65884bb0e4e9f1373b80
Multirious <multirious@outlook.com>
2025-11-28 17:28:46 +0700

    Move modules/home-manager/configurations.nix into modules/configurations.nix

--- modules/configurations.nix
+++ modules/configurations.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
 let
   classAspects = lib.types.lazyAttrsOf (
     lib.types.submodule {
@@ -11,20 +16,50 @@ let
       };
     }
   );
-  useToImports = class: use: builtins.map (useAspectName: class."${useAspectName}".module) use;
+  useToImports =
+    className: use:
+    builtins.map (useAspectName: config.flake.modules."${className}"."${useAspectName}") use;
   toFlakeModules =
-    class:
+    className:
     lib.mapAttrs (aspectName: aspect: {
-      imports = [ aspect.module ] ++ (useToImports class aspect.use);
-    }) class;
+      imports = [ aspect.module ] ++ (useToImports className aspect.use);
+    }) config."${className}";
 in
 {
   options = {
     homeManager = lib.mkOption { type = classAspects; };
+    configurations.homeManager = lib.mkOption {
+      type = lib.types.lazyAttrsOf (
+        lib.types.submodule {
+          options = {
+            system = lib.mkOption { type = lib.types.str; };
+            module = lib.mkOption { type = lib.types.deferredModule; };
+            use = lib.mkOption { type = lib.types.listOf lib.types.str; };
+          };
+        }
+      );
+    };
+
     nixos = lib.mkOption { type = classAspects; };
   };
   config = {
-    flake.modules.homeManager = toFlakeModules config.homeManager;
-    flake.modules.nixos = toFlakeModules config.nixos;
+    flake.modules.homeManager = toFlakeModules "homeManager";
+    flake.homeConfigurations =
+      let
+        toHomeManagerConfigurations =
+          configurationName: configuration:
+          inputs.home-manager.lib.homeManagerConfiguration {
+            pkgs = inputs.nixpkgs.legacyPackages."${configuration.system}";
+            modules = [
+              configuration.module
+              {
+                imports = useToImports "homeManager" configuration.use;
+              }
+            ];
+          };
+      in
+      lib.mapAttrs toHomeManagerConfigurations config.configurations.homeManager;
+
+    flake.modules.nixos = toFlakeModules "nixos";
   };
 }
