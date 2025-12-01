7f373f308b2ef6fbdb5603bae181d7253528cf2b
Multirious <multirious@outlook.com>
2025-12-01 15:36:56 +0700

    Add lib with fetchFromItch

---
+++ lib/fetchFromItch/default.nix
@@ -0,0 +1,43 @@
+# https://github.com/emanueljg/nix-fetchers
+{
+  lib,
+  stdenvNoCC,
+  cacert,
+  fetchurl,
+  curl,
+  jq,
+}:
+{
+  creator,
+  game,
+  uploadName,
+  apiKeyPath ? "/run/secrets/fetchFromItch",
+  hash ? lib.fakeHash,
+  name ? "${creator}-${game}-${uploadName}",
+  ...
+}@args:
+stdenvNoCC.mkDerivation (
+  {
+    inherit
+      creator
+      game
+      uploadName
+      apiKeyPath
+      hash
+      name
+      ;
+    builder = ./builder.sh;
+    buildInputs = [
+      jq
+      curl
+    ];
+    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+    outputHash = hash;
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  }
+  // builtins.removeAttrs args [
+    "hash"
+  ]
+)
