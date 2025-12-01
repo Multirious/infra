cefc3d2452d8f0a022d984b799e6c3f9715e6137
Multirious <multirious@outlook.com>
2025-12-01 09:25:50 +0700

    WIP fake home

---
+++ modules/fake-home.nix
@@ -0,0 +1,63 @@
+top:
+let
+  description = ''
+    Create alternate home directory for programs not following XDG specification.
+    Any program using this module must be wrapped with HOME environment variable pointing
+    to the fake home directory defined by this module.
+  '';
+in
+{
+  inherit description;
+  configurations.homeManager.peach.use = [ "fakeHome" ];
+  homeManager.fakeHome.module =
+    { config, lib, ... }:
+    {
+      inherit description;
+      options = {
+        fakeHome = {
+          path = lib.mkOption {
+            type = lib.types.str;
+            default = "${config.xdg.dataHome}/fakehome";
+          };
+          home = lib.mkOption {
+            type = lib.types.lazyAttrsOf lib.types.submodule {
+              option = {
+                links = lib.mkOption {
+                  type = lib.types.lazyAttrsOf lib.types.str;
+                  default = { };
+                };
+              };
+            };
+            default = { };
+          };
+        };
+      };
+      config = {
+        fakeHome.links =
+          let
+            inherit (config) xdg;
+          in
+          {
+            ".config" = xdg.configHome;
+            ".local/state" = xdg.stateHome;
+            ".local/share" = xdg.dataHome;
+            ".cache" = xdg.cacheHome;
+          };
+        home.activation = {
+          activateFakeHome =
+            lib.hm.dag.entryAfter [ "writeBoundary" ]
+              # bash
+              ''
+                run { [ -d "${config.fakehome.root}" ] || mkdir -p "${config.fakehome.path}" }
+                ${builtins.foldl' (
+                  acc: elem:
+                  acc
+                  ++ /* bash */ ''
+                    run { [ -d "${config.fakehome.root}" ] || mkdir -p "${config.fakehome.root}" }
+                  ''
+                ) "" (config.fakehome.config.fakehome.links)}
+              '';
+        };
+      };
+    };
+}
