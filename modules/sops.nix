ac72817c7ebcaf187f2ea2356fbfc2d11e6daa3a
Multirious <multirious@outlook.com>
2025-12-01 18:57:27 +0700

    Experimenting with sops module

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,18 +1,23 @@
 top: {
   configurations.homeManager.peach.use = [ "sops" ];
   homeManager.sops.module =
-    { config, ... }:
+    { config, pkgs, ... }:
     {
       imports = [
         top.inputs.sops-nix.homeManagerModules.sops
       ];
 
-      sops = {
-        age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
-        defaultSopsFile = ../secrets.yaml;
-        secrets.example_key = {
-          path = "%r/example_key.txt";
-        };
-      };
+      # sops = {
+      #   age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+      #   defaultSopsFile = ../secrets.yaml;
+      #   secrets.itchio_api_key = {
+      #     mode = "7777"; # wtf is wrong with nix
+      #   };
+      # };
+
+      # nix.package = pkgs.nix;
+      # nix.settings.extra-sandbox-paths = [
+      #   "${config.sops.secrets.itchio_api_key.path}"
+      # ];
     };
 }
