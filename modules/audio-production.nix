4b5589d3bfc1920f4b598e82e3a9e8b88a7e51fe
Multirious <multirious@outlook.com>
2025-12-01 18:58:03 +0700

    temporary MiniMeters code

--- modules/audio-production.nix
+++ modules/audio-production.nix
@@ -3,14 +3,18 @@ top: {
   configurations.nixos.peach-asus.use = [ "audioProduction" ];
 
   homeManager.audioProduction.module =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     let
       inherit (pkgs.stdenv.hostPlatform) system;
+      # minimeters = pkgs.callPackage ../packages/MiniMeters.nix {
+      #   apiKeyPath = config.sops.secrets.itchio_api_key.path;
+      # };
     in
     {
       home.packages = [
         top.config.flake.packages."${system}".openutau
         pkgs.reaper
+        # minimeters
       ];
     };
 
