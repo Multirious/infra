da381fdc2baac3c5db2b9a6ff9ab9de7a2cb3cd8
Multirious <multirious@outlook.com>
2025-12-02 16:36:36 +0700

    Fix typo

--- modules/packages.nix
+++ modules/packages.nix
@@ -3,7 +3,7 @@ top: {
     { pkgs, ... }:
     {
       packages.openutau = pkgs.callPackage ../packages/openutau.nix { };
-      packages.sitala = pkgs.callPackage ../packages/sitala.nix.nix { };
-      packages.MiniMeters = pkgs.callPackage ../packages/MiniMeters.nix.nix { };
+      packages.sitala = pkgs.callPackage ../packages/sitala.nix { };
+      packages.MiniMeters = pkgs.callPackage ../packages/MiniMeters.nix { };
     };
 }
