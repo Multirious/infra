249e3e6e705c3d34f6569c6704683ecadcdd1489
Multirious <multirious@outlook.com>
2025-12-02 15:54:51 +0700

    secrets are complicated

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,23 +1,18 @@
 top: {
   configurations.homeManager.peach.use = [ "sops" ];
   homeManager.sops.module =
-    { config, pkgs, ... }:
+    { config, ... }:
     {
       imports = [
         top.inputs.sops-nix.homeManagerModules.sops
       ];
 
-      # sops = {
-      #   age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
-      #   defaultSopsFile = ../secrets.yaml;
-      #   secrets.itchio_api_key = {
-      #     mode = "7777"; # wtf is wrong with nix
-      #   };
-      # };
-
-      # nix.package = pkgs.nix;
-      # nix.settings.extra-sandbox-paths = [
-      #   "${config.sops.secrets.itchio_api_key.path}"
-      # ];
+      sops = {
+        age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+        defaultSopsFile = ../secrets.yaml;
+        secrets.itchio_api_key = {
+          mode = "0400";
+        };
+      };
     };
 }
