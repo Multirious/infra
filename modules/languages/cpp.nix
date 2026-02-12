429e9e185dc4f246d52819cae60067e15a5105c3
Multirious <multirious@outlook.com>
2026-02-12 14:06:17 +0700

    Make cpp use 4 width indentation

--- modules/languages/cpp.nix
+++ modules/languages/cpp.nix
@@ -5,11 +5,13 @@ top: {
       me.helix.languages =
         # toml
         ''
+          [[language]]
+          name = "cpp"
+          indent = { tab-width = 4, unit = "    " }
+
           [[grammar]]
           name = "cpp"
           source = { git = "https://github.com/tree-sitter/tree-sitter-cpp", rev = "a90f170f92d5d70e7c2d4183c146e61ba5f3a457" }
         '';
     };
 }
-
-
