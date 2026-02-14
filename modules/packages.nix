275e941653314bc9273d226feb9e79eb835195d4
Multirious <multirious@outlook.com>
2026-02-14 20:42:21 +0700

    Fix packages module not allowing unfree

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,7 +1,13 @@
 top: {
   perSystem =
-    { pkgs, ... }:
+    { pkgs, system, ... }:
     {
+      _module.args.pkgs = import top.inputs.nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
       packages = import ../packages/default.nix { inherit pkgs; };
     };
 }
