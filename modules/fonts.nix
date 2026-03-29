1130b9ac5a98269a203e416f548b3b8550ef6a2f
Multirious <multirious@outlook.com>
2026-03-29 15:58:07 +0700

    Improve font config for asian characters

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,26 +1,47 @@
-top: {
+top:
+let
+  fontPkgs =
+    pkgs: with pkgs; [
+      nerd-fonts.roboto-mono
+      noto-fonts
+      newcomputermodern
+      wqy_zenhei
+    ];
+  defaultFonts = {
+    serif = [
+      "NotoSans"
+      "newcomputermodern"
+    ];
+    sansSerif = [ "NotoSerif" ];
+    monospace = [ "RobotoMono" ];
+  };
+in
+{
   configurations.nixos.peach-asus.use = [ "fonts" ];
+  configurations.homeManager.peach.use = [ "fonts" ];
+
+  homeManager.fonts.module =
+    { pkgs, lib, ... }:
+    {
+      options = {
+        fonts.packages = lib.mkOption {
+          type = lib.types.listOf lib.types.package;
+          description = "List of font packages. To only be used in my homemanager config";
+        };
+      };
+      config = {
+        fonts.fontconfig.enable = true;
+        fonts.fontconfig.defaultFonts = defaultFonts;
+        fonts.packages = fontPkgs pkgs;
+      };
+    };
 
   nixos.fonts.module =
     { pkgs, ... }:
     {
       fonts = {
-        packages = with pkgs; [
-          nerd-fonts.roboto-mono
-          noto-fonts
-          newcomputermodern
-        ];
-
-        fontconfig = {
-          defaultFonts = {
-            serif = [
-              "NotoSans"
-              "newcomputermodern"
-            ];
-            sansSerif = [ "NotoSerif" ];
-            monospace = [ "RobotoMono" ];
-          };
-        };
+        packages = fontPkgs pkgs;
+        fontconfig.defaultFonts = defaultFonts;
       };
       console = {
         earlySetup = true;
