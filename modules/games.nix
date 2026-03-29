d40f7735d69f259a0aa932719c8b42eee33ace47
Multirious <multirious@outlook.com>
2026-03-29 15:59:35 +0700

    Uncommented prismlauncher

--- modules/games.nix
+++ modules/games.nix
@@ -19,9 +19,10 @@ top: {
       home.packages = [
         steam
         pkgs.protontricks
-        # pkgs.prismlauncher
+        pkgs.prismlauncher
+        # top.inputs.noita_entangled_worlds.packages.${pkgs.stdenv.system}.noita-proxy
       ];
-      xdg.dataFile."Steam/compatibilitytools.d/GE-Proton10-25".source = builtins.fetchTarball {
+      xdg.dataFile."Steam/compatibilitytools.d/GE-Proton10-25".source = fetchTarball {
         url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-25/GE-Proton10-25.tar.gz";
         sha256 = "0jb3j9ida2962nmpihpjbp5da70q20hls1r0sn17pdii0ghjiaa4";
       };
