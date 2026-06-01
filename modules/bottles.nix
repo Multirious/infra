3fcb1bcffa7d1ad9feeabd0f59a4f90434a10e86
Multirious <multirious@outlook.com>
2026-06-01 21:41:54 +0700

    Temp bottles fix

--- modules/bottles.nix
+++ modules/bottles.nix
@@ -4,6 +4,15 @@ top: {
   homeManager.bottles.module =
     { pkgs, ... }:
     {
+      nixpkgs.overlays = [
+        # Skipping tests while upstream sorts it out, revert once
+        # Hydra consistently builds openldap green.
+        (final: prev: {
+          openldap = prev.openldap.overrideAttrs (_: {
+            doCheck = false;
+          });
+        })
+      ];
       home.packages = [
         (pkgs.bottles.override {
           removeWarningPopup = true;
