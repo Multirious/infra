10ad50b930cb88f1c9ff77050ff1a22e039c27d2
Multirious <multirious@outlook.com>
2026-07-03 21:05:56 +0700

    Remove rename-workspace-menu script

--- modules/hyprland/scripts.nix
+++ modules/hyprland/scripts.nix
@@ -2,24 +2,24 @@ top: {
   homeManager.hyprland.module =
     { ... }:
     {
-      xdg.configFile."hypr/scripts/rename-workspace-menu" = {
-        executable = true;
-        text =
-          # bash
-          ''
-            #!/usr/bin/env bash
+      # xdg.configFile."hypr/scripts/rename-workspace-menu" = {
+      #   executable = true;
+      #   text =
+      #     # bash
+      #     ''
+      #       #!/usr/bin/env bash
 
-            choices="obsidian
-            terminal
-            "
-            choosen="$(echo -e "$choices" | wofi -d)"
-            if [ -z "$choosen" ]; then
-              exit
-            fi
-            current_workspace=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .activeWorkspace.id')
-            hyprctl dispatch renameworkspace "$current_workspace" "$choosen"
-          '';
-      };
+      #       choices="obsidian
+      #       terminal
+      #       "
+      #       choosen="$(echo -e "$choices" | wofi -d)"
+      #       if [ -z "$choosen" ]; then
+      #         exit
+      #       fi
+      #       current_workspace=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .activeWorkspace.id')
+      #       hyprctl dispatch renameworkspace "$current_workspace" "$choosen"
+      #     '';
+      # };
       xdg.configFile."hypr/scripts/close-current-special-workspace" = {
         executable = true;
         text =
