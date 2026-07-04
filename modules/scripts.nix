76911a95f8b714d3c39c360611b947b3bd3ac73f
Multirious <multirious@outlook.com>
2026-07-04 14:34:44 +0700

    Proper scripts module with options

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -2,8 +2,32 @@ top: {
   configurations.homeManager.peach.use = [ "scripts" ];
 
   homeManager.scripts.module =
-    { ... }:
+    { config, pkgs, lib, ... }:
     {
+      options = {
+        me.scripts = with lib.types; lib.mkOption {
+          type = attrsOf (submodule {
+            options = {
+              text = lib.mkOption { type = str; };
+              source = lib.mkOption { type = package; };
+            };
+          });
+        }
+      };
+      config = {
+        me.scripts = {}
+        home.packages = lib.attrValues (lib.mapAttrs (
+          scriptName: module: writeTextFile {
+            name = scriptName;
+            text = if module.text != null
+              then module.text
+              else builtins.readFile module.source;
+            executable = true;
+            destination = "/bin/${scriptName}";
+          }
+        ) config.me.scripts)
+      };
+
       home.file = {
         "scripts/debug-derivative" = {
           executable = true;
