3d36e38107718abd9bbfd30c9417b569d7713361
Multirious <multirious@outlook.com>
2026-07-04 14:51:43 +0700

    Fix scripts

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -2,45 +2,55 @@ top: {
   configurations.homeManager.peach.use = [ "scripts" ];
 
   homeManager.scripts.module =
-    { config, pkgs, lib, ... }:
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
     {
       options = {
-        me.scripts = with lib.types; lib.mkOption {
-          type = attrsOf (submodule {
-            options = {
-              text = lib.mkOption { type = str; };
-              source = lib.mkOption { type = package; };
-            };
-          });
-        }
+        me.scripts =
+          with lib.types;
+          lib.mkOption {
+            type = attrsOf (submodule {
+              options = {
+                text = lib.mkOption {
+                  type = nullOr str;
+                  default = null;
+                };
+                source = lib.mkOption { type = path; };
+              };
+            });
+          };
       };
       config = {
-        me.scripts = {}
-        home.packages = lib.attrValues (lib.mapAttrs (
-          scriptName: module: writeTextFile {
-            name = scriptName;
-            text = if module.text != null
-              then module.text
-              else builtins.readFile module.source;
-            executable = true;
-            destination = "/bin/${scriptName}";
-          }
-        ) config.me.scripts)
+        home.packages = lib.attrValues (
+          lib.mapAttrs (
+            scriptName: module:
+            pkgs.writeTextFile {
+              name = scriptName;
+              text = if module.text != null then module.text else builtins.readFile module.source;
+              executable = true;
+              destination = "/bin/${scriptName}";
+            }
+          ) config.me.scripts
+        );
       };
 
-      home.file = {
-        "scripts/debug-derivative" = {
-          executable = true;
-          source = ./scripts/debug-derivative;
-        };
-        "scripts/clipboard-react-emote" = {
-          executable = true;
-          source = ./scripts/clipboard-react-emote;
-        };
-        "scripts/video-to-gif" = {
-          executable = true;
-          source = ./scripts/video-to-gif;
-        };
-      };
+      # home.file = {
+      #   "scripts/debug-derivative" = {
+      #     executable = true;
+      #     source = ./scripts/debug-derivative;
+      #   };
+      #   "scripts/clipboard-react-emote" = {
+      #     executable = true;
+      #     source = ./scripts/clipboard-react-emote;
+      #   };
+      #   "scripts/video-to-gif" = {
+      #     executable = true;
+      #     source = ./scripts/video-to-gif;
+      #   };
+      # };
     };
 }
