cd7c62c4eb0685ad7a916ca30995709b169e4267
Multirious <multirious@outlook.com>
2026-07-05 11:37:54 +0700

    nix-view script

--- modules/nix/scripts.nix
+++ modules/nix/scripts.nix
@@ -8,6 +8,10 @@ top: {
         shift
         nix shell "nixpkgs#$package" --command $*
       '';
+      me.scripts."nix-view".text = ''
+        #!/usr/bin/env bash
+        nix-build '<nixpkgs>' --attr "$1" --no-out-link
+      '';
       me.scripts."debug-derivative".source = ./debug-derivative;
     };
 }
