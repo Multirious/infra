71e30bfb606389ebd2f3dc2c902013648e9b4b3c
Multirious <multirious@outlook.com>
2026-07-07 13:38:51 +0700

    Move other overlays into the same place

--- modules/bottles.nix
+++ modules/bottles.nix
@@ -4,15 +4,6 @@ top: {
   homeManager.bottles.module =
     { pkgs, ... }:
     {
-      nixpkgs.overlays = [
-        # Skipping tests while upstream sorts it out, revert once
-        # Hydra consistently builds openldap green.
-        (final: prev: {
-          openldap = prev.openldap.overrideAttrs (_: {
-            doCheck = false;
-          });
-        })
-      ];
       home.packages = [
         (pkgs.bottles.override {
           removeWarningPopup = true;
--- modules/packages.nix
+++ modules/packages.nix
@@ -4,7 +4,19 @@ top: {
     {
       _module.args.pkgs = import top.inputs.nixpkgs {
         inherit system;
-        overlays = [ (import top.inputs.rust-overlay) ];
+        overlays = [
+          (import top.inputs.rust-overlay)
+
+          # For the bottles module
+          #
+          # Skipping tests while upstream sorts it out, revert once
+          # Hydra consistently builds openldap green.
+          (final: prev: {
+            openldap = prev.openldap.overrideAttrs (_: {
+              doCheck = false;
+            });
+          })
+        ];
         config = {
           allowUnfree = true;
         };
