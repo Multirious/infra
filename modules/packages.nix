40930a7d7ac32c49203ee538a8c66d9705fc2a8d
Multirious <multirious@outlook.com>
2026-07-08 21:46:00 +0700

    Remove openldap overlay

--- modules/packages.nix
+++ modules/packages.nix
@@ -6,16 +6,6 @@ top: {
         inherit system;
         overlays = [
           (import top.inputs.rust-overlay)
-
-          # For the bottles module
-          #
-          # Skipping tests while upstream sorts it out, revert once
-          # Hydra consistently builds openldap green.
-          (final: prev: {
-            openldap = prev.openldap.overrideAttrs (_: {
-              doCheck = false;
-            });
-          })
         ];
         config = {
           allowUnfree = true;
