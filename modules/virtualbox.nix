26d1ef9d1616514197c74c88954f441ceaa7e522
Multirious <multirious@outlook.com>
2026-07-10 18:19:22 +0700

    Update virtualbox

--- modules/virtualbox.nix
+++ modules/virtualbox.nix
@@ -2,12 +2,20 @@ top: {
   configurations.nixos.peach-asus.use = [ "virtualbox" ];
 
   nixos.virtualbox.module =
-    { ... }:
+    { pkgs, ... }:
     {
-      virtualisation.virtualbox.host.enable = true;
-      virtualisation.virtualbox.guest.vboxsf = true;
-      virtualisation.virtualbox.guest.enable = true;
-      virtualisation.virtualbox.guest.clipboard = true;
+      virtualisation.virtualbox = {
+        host.enable = true;
+        host.package = pkgs.virtualbox.overrideAttrs (
+          final: prev: {
+            virtualboxVersion = "7.2.12";
+            virtualboxSha256 = "sha256-ZKSENnfkIBDneZ6VGIP7vvxWvyvBYuSXDt6gTxQviyU=";
+          }
+        );
+        guest.vboxsf = true;
+        guest.enable = true;
+        guest.clipboard = true;
+      };
 
       boot.kernelParams = [
         "kvm.enable_virt_at_load=0"
