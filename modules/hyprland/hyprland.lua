20f63dd7c82e46da40d7cd362fda5d03c6cd45d5
Multirious <multirious@outlook.com>
2026-07-15 16:06:16 +0700

    Dont follow window when moved hyprland

--- modules/hyprland/hyprland.lua
+++ modules/hyprland/hyprland.lua
@@ -66,7 +66,7 @@ for i = 1,10
 do
   local key = i % 10
   hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
-  hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+  hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i, follow = false }))
 end
 
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag())
